c74a557fc3408d1f99d5e974a6bf4a40622c521d
Private <atef.aziz.eg@gmail.com>
2022-08-17 16:01:41 +0200

    dfsdfsdf

--- src/FishingTrip.Domain/FishingTripDomain/Profile.cs
+++ src/FishingTrip.Domain/FishingTripDomain/Profile.cs
@@ -6,13 +6,15 @@ namespace FishingTrip.Domain.FishingTripDomain
     public record UserProfile
     {
         [MaxLength(100)]
-        public string Email { get; init; }
+        public string? Email { get; init; }
+        [Required]
         [Column(TypeName = "jsonb")]
         public UserProfile? ProfileDetails { get; init; }
 
         [Key]
         [MaxLength(15)]
-        public string PhoneNumber { get; init; }
+        [Required]
+        public string? PhoneNumber { get; init; }
 
         public long? DefaultUserId { get; init; }
         [ForeignKey(nameof(DefaultUserId))]
