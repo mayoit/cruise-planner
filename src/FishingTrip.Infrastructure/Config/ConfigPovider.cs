9de24c15b863f5eb860c54be67fa4160e4ee9003
MAYO Tech <88550162+atef-aziz-eg@users.noreply.github.com>
2022-08-17 15:10:08 +0200

    Update ConfigPovider.cs

--- src/FishingTrip.Infrastructure/Config/ConfigPovider.cs
+++ src/FishingTrip.Infrastructure/Config/ConfigPovider.cs
@@ -5,6 +5,6 @@ namespace FishingTrip.Infrastructure.Config
 {
     internal class ConfigProvider : IConfigProvider
     {
-        public string FishingTripDbConnectionString { get; } = "User ID=postgres;Password=admin;Host=localhost;Port=5432;Database=newsimpleFishing;Pooling=true";
+        public string FishingTripDbConnectionString { get; } = @"User ID=postgres;Password=admin;Host=localhost;Port=5432;Database=newsimpleFishing;Pooling=true";
     }
 }
